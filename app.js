a19eb8588c0e793447b8b96c95967b4708b1a41e
eldr0n-git <eldron@gmail.com>
2022-07-28 00:04:21 +0600

    feat: get user Geolocation and fetches for more details

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 "use strict";
+
+let mylat, mylong;
+
+const myPosition = () => {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    let mylat = position.coords.latitude;
+    let mylong = position.coords.longitude;
+    console.log(`${mylat}, ${mylong}`);
+    fetch(`http://geocode.xyz/${mylat},${mylong}?geoit=json&auth=218699383617088100122x124626`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  });
+};
+const getDataPosition = (mylat, mylong) => {};
+const btnHandler = document.getElementById("getDataRun");
+btnHandler ? btnHandler.addEventListener("click", myPosition()) : console.log("err");
